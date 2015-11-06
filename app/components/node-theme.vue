cd6372904bcab9a73178b28c510dd91f44a8acbe
janschoenherr <jan@yootheme.com>
2015-11-06 13:52:21 +0100

    use v-else

--- app/components/node-theme.vue
+++ app/components/node-theme.vue
@@ -2,26 +2,28 @@
 
     <div class="uk-form-horizontal">
 
-        <div class="uk-form-row" v-if="node.type === 'page'">
-            <span class="uk-form-label">{{ 'Title' | trans }}</span>
-            <div class="uk-form-controls uk-form-controls-text">
-                <label><input type="checkbox" v-model="node.theme.title_hide"> {{ 'Hide Title' | trans }}</label>
+        <template v-if="node.type === 'page'">
+            <div class="uk-form-row">
+                <span class="uk-form-label">{{ 'Title' | trans }}</span>
+                <div class="uk-form-controls uk-form-controls-text">
+                    <label><input type="checkbox" v-model="node.theme.title_hide"> {{ 'Hide Title' | trans }}</label>
+                </div>
             </div>
-        </div>
 
-        <div class="uk-form-row" v-if="node.type === 'page'">
-            <span class="uk-form-label">{{ 'Title Size' | trans }}</span>
-            <div class="uk-form-controls uk-form-controls-text">
-                <label><input type="checkbox" v-model="node.theme.title_large"> {{ 'Extra large title.' | trans }}</label>
+            <div class="uk-form-row">
+                <span class="uk-form-label">{{ 'Title Size' | trans }}</span>
+                <div class="uk-form-controls uk-form-controls-text">
+                    <label><input type="checkbox" v-model="node.theme.title_large"> {{ 'Extra large title.' | trans }}</label>
+                </div>
             </div>
-        </div>
 
-        <div class="uk-form-row" v-if="node.type === 'page'">
-            <span class="uk-form-label">{{ 'Alignment' | trans }}</span>
-            <div class="uk-form-controls uk-form-controls-text">
-                <label><input type="checkbox" v-model="node.theme.alignment"> {{ 'Center the title and content.' | trans }}</label>
+            <div class="uk-form-row">
+                <span class="uk-form-label">{{ 'Alignment' | trans }}</span>
+                <div class="uk-form-controls uk-form-controls-text">
+                    <label><input type="checkbox" v-model="node.theme.alignment"> {{ 'Center the title and content.' | trans }}</label>
+                </div>
             </div>
-        </div>
+        </template>
 
         <div class="uk-form-row">
             <label for="form-class" class="uk-form-label">{{ 'HTML Class' | trans }}</label>
